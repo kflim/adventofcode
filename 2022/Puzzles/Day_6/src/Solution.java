f063f731b42804e6fd381d9747033248cd258057
kflim <redbulldown@gmail.com>
2022-12-06 15:15:18 +0800

    Reduce unnecessary code

--- 2022/Puzzles/Day_6/src/Solution.java
+++ 2022/Puzzles/Day_6/src/Solution.java
@@ -2,13 +2,13 @@ public class Solution {
 
     public static void solvePartOne() {
         String signal = Util.readLine("input.txt");
-        int firstMarker = Util.findFirstPacketMarker(signal);
+        int firstMarker = Util.findFirstSectionMarker(signal, 4);
         System.out.println("The first start-of-packet marker is : " + firstMarker);
     }
 
     public static void solvePartTwo() {
         String signal = Util.readLine("input.txt");
-        int firstMarker = Util.findFirstMessageMarker(signal);
+        int firstMarker = Util.findFirstSectionMarker(signal, 14);
         System.out.println("The first start-of-message marker is : " + firstMarker);
     }
 
--- 2022/Puzzles/Day_6/src/Util.java
+++ 2022/Puzzles/Day_6/src/Util.java
@@ -15,10 +15,9 @@ public class Util {
         return "";
     }
 
-    public static int findFirstPacketMarker(String signal) {
+    public static int findFirstSectionMarker(String signal, int length) {
         Map<Character, Integer> characterIndexes = new HashMap<>();
         int signalLength = signal.length();
-        int packetLength = 4;
 
         for (int i = 0, start = 0; i < signalLength; i++) {
             char c = signal.charAt(i);
@@ -27,28 +26,7 @@ public class Util {
                     start = characterIndexes.get(c) + 1;
                 }
             }
-            if (i - start + 1 == packetLength) {
-                return i + 1;
-            }
-            characterIndexes.put(c, i);
-        }
-
-        return signalLength;
-    }
-
-    public static int findFirstMessageMarker(String signal) {
-        Map<Character, Integer> characterIndexes = new HashMap<>();
-        int signalLength = signal.length();
-        int packetLength = 14;
-
-        for (int i = 0, start = 0; i < signalLength; i++) {
-            char c = signal.charAt(i);
-            if (characterIndexes.containsKey(c)) {
-                if (characterIndexes.get(c) >= start) {
-                    start = characterIndexes.get(c) + 1;
-                }
-            }
-            if (i - start + 1 == packetLength) {
+            if (i - start + 1 == length) {
                 return i + 1;
             }
             characterIndexes.put(c, i);
